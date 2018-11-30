13801de08068f82c0f44c165d8befb47e4ca79d8
totoseriapovlup <totoseriapov@yandex.ua>
2018-11-30 17:12:29 +0200

    function getStudents

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
-var students = [
-    {
-	name: 'Петя',
-	surname: 'Горох',
-	email: 'goroh@gmail.com'
-    },
-    {
-	name: 'Вова',
-	surname: 'Пупко',
-	email: 'pupko@yandex.ru'
-    },
-    {
-	name: 'Воланд',
-	surname: 'Бампер',
-	email: 'voland@yahoo.com'
-    }
-];
+function getStudents() {
+    var students = [
+	{
+	    name: 'Петя',
+	    surname: 'Горох',
+	    email: 'goroh@gmail.com'
+	},
+	{
+	    name: 'Вова',
+	    surname: 'Пупко',
+	    email: 'pupko@yandex.ru'
+	},
+	{
+	    name: 'Воланд',
+	    surname: 'Бампер',
+	    email: 'voland@yahoo.com'
+	}
+    ];
+    return students;
+}
--- js/students.js
+++ js/students.js
@@ -1,5 +1,5 @@
 $('#students').append('<ul></ul>');
-$(students).each(function (i, student) {
+$(getStudents()).each(function (i, student) {
     $('#students ul').append('<li>' + student.name + ' ' + student.surname + ' ' + student.email + '</li>');
 }
 );
