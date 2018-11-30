c5498022cbd7b6095c73d675a3b0c9516d78841d
user <Global@mail.ua>
2018-11-30 16:55:45 +0200

    edit students.js

--- js/students.js
+++ js/students.js
@@ -3,8 +3,7 @@ $(document).ready(function () {
     var ul = document.createElement('ul');
     $.each(students, function(i, item) {        
         var li = document.createElement('li');
-        for(student in item){  
-            console.log(item['name']);
+        for(student in item){            
             $(li).text(item['name'] + ' ' + item['surname'] + ' ' + item['email']);
             $(li).appendTo(ul);
         }
