e77d552a1e6c8a514488a0ddb293ce47280ef619
user <Global@mail.ua>
2018-12-03 17:45:57 +0200

    onsubmit

--- app/views/page_main_index_view.php
+++ app/views/page_main_index_view.php
@@ -1,7 +1,7 @@
 <h2>Students</h2>
 <div id="students"></div>
 <div id="modalWindow">
-    <form onsubmit="ajax" action="form">
+    <form id="add_student_form" method="post">
 	<input type="text" name="name" placeholder="name"/>
 	<input type="text" name="surname" placeholder="surname"/>
 	<input type="email" name="email" placeholder="email"/>
@@ -9,10 +9,5 @@
     </form>
 </div>
 <button id="btn">add student</button>
-<script>
-    $("#btn").click(function(){
-	$('div#modalWindow').css('display', 'block');
-    });
-</script>
 <script src="/js/main.js" type="text/javascript"></script>
 <script src="/js/students.js" type="text/javascript"></script>
--- js/students.js
+++ js/students.js
@@ -1,10 +1,18 @@
 getStudents();
+$("#btn").click(function () {
+    $('div#modalWindow').css('display', 'block');
+});
+$("#add_student_form").submit(function () {
+    var xhr = new XMLHttpRequest();
 
-var xhr = new XMLHttpRequest();
+    var body = 'name=' + encodeURIComponent($name) + '&surname=' + encodeURIComponent($surname) + '&email=' + encodeURIComponent($email);
+    
 
-var body = 'name=' + encodeURIComponent($name) + '&surname=' + encodeURIComponent($surname) + '&email=' + encodeURIComponent($email);;
+    xhr.open("POST", 'form', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
-xhr.open("POST", 'form', true);
-xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(body);
+    return false;
+});
 
-xhr.send(body);
+
