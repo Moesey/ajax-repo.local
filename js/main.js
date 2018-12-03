0c3d17a4ab06b7ab654a07c95988072a91e4d93c
totoseriapovlup <totoseriapov@yandex.ua>
2018-12-03 16:48:52 +0200

    ajax

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
+function showStudents(students) {
+    $('#students').append('<ul></ul>');
+    $(students).each(function (i, student) {
+	$('#students ul').append('<li>' + student.name + ' ' + student.surname + ' ' + student.email + '</li>');
+    }
+    );
+}
 function getStudents() {
-    var students = [
-	{
-	    name: 'Петя',
-	    surname: 'Горох',
-	    email: 'goroh@gmail.com'
-	},
-	{
-	    name: 'Вова',
-	    surname: 'Пупко',
-	    email: 'pupko@yandex.ru'
-	},
-	{
-	    name: 'Воланд',
-	    surname: 'Бампер',
-	    email: 'voland@yahoo.com'
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', location.origin + '/api/index');
+    xhr.onreadystatechange = function () {
+	if (xhr.readyState === 4 && xhr.status === 200) {
+	    var json = xhr.responseText;
+	    var students = JSON.parse(json);
+	    if (students) {
+		showStudents(students)
+	    }
 	}
-    ];
-    return students;
+    };
+    xhr.send();
 }
--- js/students.js
+++ js/students.js
@@ -1,6 +1,2 @@
-$('#students').append('<ul></ul>');
-$(getStudents()).each(function (i, student) {
-    $('#students ul').append('<li>' + student.name + ' ' + student.surname + ' ' + student.email + '</li>');
-}
-);
+getStudents();
 
