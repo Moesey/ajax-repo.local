2d5345aab533040bf1e654d840701d52a1e3a5ab
mrchuguy <mr.chuguy@gmail.com>
2018-12-05 12:09:07 +0200

    post ajax

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -1,22 +1,28 @@
 <?php
 
-class ApiController extends Controller {
+class ApiController extends Controller
+{
 
-    public function action_index() {
-	$this->model = new ApiModel();
-	$users = $this->model->getUsers();
-	$json = json_encode($users);
-	header('Content-type: application/json; charset=utf-8');
-	echo $json;
+    public function action_index()
+    {
+        $this->model = new ApiModel();
+        $users = $this->model->getUsers();
+        $json = json_encode($users);
+        header('Content-type: application/json; charset=utf-8');
+        echo $json;
     }
 
-    public function action_add($name, $surname, $email) {
-	if ($name === null || $surname === null || $email === NULL) {
-	    return false;
-	} else {
-	    $this->model = new ApiModel();
-	    $this->model->addStudent($name, $surname, $email);
-	}
+    public function action_add()
+    {
+        $name = filter_input(INPUT_POST, 'name');
+        $surname = filter_input(INPUT_POST, 'surname');
+        $email = filter_input(INPUT_POST, 'email');
+        if ($name === null || $surname === null || $email === NULL) {
+            return false;
+        } else {
+            $this->model = new ApiModel();
+            $this->model->addStudent($name, $surname, $email);
+        }
     }
 
 }
--- js/students.js
+++ js/students.js
@@ -3,12 +3,15 @@ $("#btn").click(function () {
     $('div#modalWindow').css('display', 'block');
 });
 $("#add_student_form").submit(function () {
+    var
+        name = $(this).find('input[name="name"]').val(),
+        surname = $(this).find('input[name="surname"]').val(),
+        email = $(this).find('input[type="email"]').val();
     var xhr = new XMLHttpRequest();
 
-    var body = 'name=' + encodeURIComponent($name) + '&surname=' + encodeURIComponent($surname) + '&email=' + encodeURIComponent($email);
-    
+    var body = 'name=' + encodeURIComponent(name) + '&surname=' + encodeURIComponent(surname) + '&email=' + encodeURIComponent(email);
 
-    xhr.open("POST", 'form', true);
+    xhr.open("POST", location.origin + '/api/add', true);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
     xhr.send(body);
