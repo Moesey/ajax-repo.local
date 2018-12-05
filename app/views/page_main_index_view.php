8eef16a7974386e807f10c936c9d44eefb6f6a09
user <user@user.com>
2018-12-05 14:56:32 +0200

    mod

--- app/views/page_main_index_view.php
+++ app/views/page_main_index_view.php
@@ -1,4 +1,13 @@
 <h2>Students</h2>
 <div id="students"></div>
+<div id="modalWindow">
+    <form id="add_student_form" method="post">
+	<input type="text" name="name" placeholder="name"/>
+	<input type="text" name="surname" placeholder="surname"/>
+	<input type="email" name="email" placeholder="email"/>
+	<input type="submit" value="add"/>
+    </form>
+</div>
+<button id="btn">add student</button>
 <script src="/js/main.js" type="text/javascript"></script>
 <script src="/js/students.js" type="text/javascript"></script>
